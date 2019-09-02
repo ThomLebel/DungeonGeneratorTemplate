a2196633267e2b10136885b56c8a06dc57695d2c
tlebel <thom.lebel@orange.fr>
2019-09-02 15:03:01 +0200

    Initial commit

---
+++ DungeonGeneratorTemplate/Assets/Scripts/Room.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+	public int type;
+	public bool isMainPath = false;
+	public Vector2 position;
+
+	public void RoomDestruction()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ DungeonGeneratorTemplate/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public void SetRoomInformation(bool isMainPath, Vector2 position)
+	{
+		Room roomScript = GetComponentInChildren<Room>();
+		roomScript.isMainPath = isMainPath;
+		roomScript.position = position;
+	}
+}
